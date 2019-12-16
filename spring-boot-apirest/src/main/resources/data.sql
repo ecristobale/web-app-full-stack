cc4ca3ac4d6b6701652a185d43b9946b71d79114
ecristobale <edu_ce_1988@hotmail.com>
2019-12-16 14:50:22 +0100

    CLIENTES H2 table created with the following fields: ID, NOMBRE, APELLIDO, EMAIL, CREATED_AT Added H2 connection configuration to application.properties

---
+++ spring-boot-apirest/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO CLIENTE (NOMBRE, APELLIDO, EMAIL) 
+VALUES 
+('Eduardo', 'Cristóbal', 'unemail@gmail.com'),
+('Jonh', 'Doe', 'johndoe@gmail.com'),
+('Mary', 'Doe', 'marydoe@gmailcom'),
+('Alex', 'Harrys', 'aharrys@gmailcom'),
+('Tristana', 'Lol', 'adctrist@gmailcom'),
+('Matt', 'John', 'mj@gmailcom');
