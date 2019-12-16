55e4db4736b85ff78f90ff6b3ec402c128fbcee8
ecristobale <edu_ce_1988@hotmail.com>
2019-12-16 14:01:40 +0100

    Initial Spring Boot project with the following project dependencies: - Web - JPA - H2 - DevTools

---
+++ spring-boot-apirest/src/main/java/com/ecristobale/spring/boot/apirest/SpringBootApirestApplication.java
@@ -0,0 +1,13 @@
+package com.ecristobale.spring.boot.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApirestApplication.class, args);
+	}
+
+}
---
+++ spring-boot-apirest/src/test/java/com/ecristobale/spring/boot/apirest/SpringBootApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecristobale.spring.boot.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
