7977ed6735db19d5041e3bd797c7560050bef2d8
ecristobale <edu_ce_1988@hotmail.com>
2019-12-16 11:30:17 +0100

    Decoupled clientes component by move clientes array to an external class: clientes.json.ts

---
+++ angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,14 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'John', apellido: 'Doe', email: 'johndoe@johndoe.es', createdAt: '16-12-2019'},
+    {id: 2, nombre: 'Mary', apellido: 'Doe', email: 'mary@johndoe.es', createdAt: '16-12-2019'},
+    {id: 3, nombre: 'Edward', apellido: 'Doe', email: 'edward@johndoe.es', createdAt: '16-12-2019'},
+    {id: 4, nombre: 'George', apellido: 'Doe', email: 'george@johndoe.es', createdAt: '16-12-2019'},
+    {id: 5, nombre: 'Alex', apellido: 'Doe', email: 'alex@johndoe.es', createdAt: '16-12-2019'}/*,
+    {id: 6, nombre: 'Mad', apellido: 'Doe', email: 'mad@johndoe.es', createdAt: '16-12-2019'},
+    {id: 7, nombre: 'Matt', apellido: 'Doe', email: 'matt@johndoe.es', createdAt: '16-12-2019'},
+    {id: 8, nombre: 'Tristana', apellido: 'Doe', email: 'trist@johndoe.es', createdAt: '16-12-2019'},
+    {id: 9, nombre: 'Harry', apellido: 'Doe', email: 'harr@johndoe.es', createdAt: '16-12-2019'},
+    {id: 10, nombre: 'William', apellido: 'Doe', email: 'wil@johndoe.es', createdAt: '16-12-2019'}*/
+  ];
