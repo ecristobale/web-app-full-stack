44f5a143916828badbb924b61610b9db18ae81b9
ecristobale <edu_ce_1988@hotmail.com>
2019-12-16 17:17:56 +0100

    Enable Angular domain with CORS for Rest Api (all operations: GET, POST, PUT and DELETE).

--- spring-boot-apirest/src/main/java/com/ecristobale/spring/boot/apirest/controllers/ClienteRestController.java
+++ spring-boot-apirest/src/main/java/com/ecristobale/spring/boot/apirest/controllers/ClienteRestController.java
@@ -3,6 +3,7 @@ package com.ecristobale.spring.boot.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ecristobale.spring.boot.apirest.models.entity.Cliente;
 import com.ecristobale.spring.boot.apirest.models.services.IClienteService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
