438a24def4754ed6199f3ab96b9fdaebc0b44f6f
ecristobale <edu_ce_1988@hotmail.com>
2020-01-25 19:59:01 +0100

    Added customized alert using SweetAlert2 for showing info when a client is created.

--- angular/clientes-app/src/app/clientes/form.component.ts
+++ angular/clientes-app/src/app/clientes/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import { Router } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -12,16 +13,17 @@ export class FormComponent implements OnInit {
   private titulo = 'Crear cliente';
 
   constructor(private clienteService: ClienteService,
-    private router: Router) { }
+              private router: Router) { }
 
   ngOnInit() {
   }
 
   create(): void {
-    console.log('Clicked');
-    console.log(this.cliente);
     this.clienteService.create(this.cliente).subscribe(
-      response => this.router.navigate(['/clientes'])
+      cliente => {
+        this.router.navigate(['/clientes']);
+        swal.fire('Nuevo cliente', `Cliente ${cliente.nombre} creado con éxito`, 'success');
+      }
     );
   }
 
