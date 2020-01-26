ef889b8a36f3decb086e54870c7591324298a80c
ecristobale <edu_ce_1988@hotmail.com>
2020-01-26 16:20:07 +0100

    Adding validation to the request body received in rest api by using Binding Result and javax validation annotations.

--- spring-boot-apirest/src/main/java/com/ecristobale/spring/boot/apirest/controllers/ClienteRestController.java
+++ spring-boot-apirest/src/main/java/com/ecristobale/spring/boot/apirest/controllers/ClienteRestController.java
@@ -3,11 +3,15 @@ package com.ecristobale.spring.boot.apirest.controllers;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +20,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecristobale.spring.boot.apirest.models.entity.Cliente;
@@ -56,9 +59,18 @@ public class ClienteRestController {
 	}
 	
 	@PostMapping("/clientes")
-	public ResponseEntity<?> create(@RequestBody Cliente cliente) {
+	public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente, BindingResult result) {
 		Cliente newCliente = null;
 		Map<String, Object> response = new HashMap<>();
+		
+		if(result.hasErrors()) {
+			List<String> errors = result.getFieldErrors().stream()
+					.map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage()
+					).collect(Collectors.toList());
+			
+			response.put("errors", errors);
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+		}
 		
 		try {
 			newCliente = clienteService.save(cliente);
@@ -74,10 +86,20 @@ public class ClienteRestController {
 	}
 	
 	@PutMapping("/clientes/{id}")
-	public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Cliente cliente) {
+	public ResponseEntity<?> update(@Valid @PathVariable Long id, @RequestBody Cliente cliente,
+			BindingResult result) {
 		Cliente clienteActual = clienteService.findById(id);
 		Cliente clienteUpdated = null;
 		Map<String, Object> response = new HashMap<>();
+		
+		if(result.hasErrors()) {
+			List<String> errors = result.getFieldErrors().stream()
+					.map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage()
+					).collect(Collectors.toList());
+			
+			response.put("errors", errors);
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+		}
 		
 		if(clienteActual == null) {
 			response.put("mensaje", "Error al actualizar. El cliente ID: ".concat(id.toString())
--- spring-boot-apirest/src/main/java/com/ecristobale/spring/boot/apirest/models/entity/Cliente.java
+++ spring-boot-apirest/src/main/java/com/ecristobale/spring/boot/apirest/models/entity/Cliente.java
@@ -12,6 +12,9 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "clientes")
@@ -21,12 +24,17 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotEmpty
+	@Size(min = 4, max = 12)
 	@Column(nullable = false)
 	private String nombre;
-	
+
+	@NotEmpty
 	@Column(nullable = false)
 	private String apellido;
 
+	@NotEmpty
+	@Email
 	@Column(nullable = false, unique = true)
 	private String email;
 	
