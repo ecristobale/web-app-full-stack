537129781d09890d29e1e13fbe85613a8cf89fa0
ecristobale <edu_ce_1988@hotmail.com>
2020-01-30 23:52:16 +0100

    Added page ranges to the paginator.

--- angular/clientes-app/src/app/paginator/paginator.component.ts
+++ angular/clientes-app/src/app/paginator/paginator.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
   templateUrl: './paginator.component.html'
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit, OnChanges {
   @Input() paginator: any;
   pages: number[];
+
+  from: number;
+  to: number;
 
   constructor() { }
 
   ngOnInit() {
-    this.pages = new Array(this.paginator.totalPages).fill(0).map((valor, indice) => indice + 1);
+    this.initPaginator();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const updatedPaginator = changes.paginator;
+
+    if (updatedPaginator.previousValue) {
+      this.initPaginator();
+    }
+  }
+
+  private initPaginator(): void {
+    this.from = Math.min(Math.max(1, this.paginator.number - 4), this.paginator.totalPages - 5);
+    this.to = Math.max(Math.min(this.paginator.totalPages, this.paginator.number + 4), 6);
+    if (this.paginator.totalPages > 5) {
+      this.pages = new Array(this.to - this.from + 1).fill(0).map((valor, indice) => indice + this.from);
+    } else {
+      this.pages = new Array(this.paginator.totalPages).fill(0).map((valor, indice) => indice + 1);
+    }
   }
 
 }
