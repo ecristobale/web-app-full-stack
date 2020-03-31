024b5efbc4a88200b76fb1c049ea0764aa94100c
ecristobale <edu_ce_1988@hotmail.com>
2020-04-01 01:26:57 +0200

    Configured cors in spring security for angular app.

--- spring-boot-apirest/src/main/java/com/ecristobale/spring/boot/apirest/auth/ResourceServerConfig.java
+++ spring-boot-apirest/src/main/java/com/ecristobale/spring/boot/apirest/auth/ResourceServerConfig.java
@@ -1,10 +1,19 @@
 package com.ecristobale.spring.boot.apirest.auth;
 
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 //Serves the requested page after token validation
 @Configuration
@@ -14,14 +23,35 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	// Security rules of our resources endpoints
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/clientes", "/api/clientes/page/**", "/api/uploads/img/**").permitAll() // public route
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/clientes", "/api/clientes/page/**", "/api/uploads/img/**", "/images/**").permitAll() // public route
 //			.antMatchers(HttpMethod.GET, "/api/clientes/{id}").hasAnyRole("ADMIN", "USER")
 //			.antMatchers(HttpMethod.POST, "/api/clientes/upload").hasAnyRole("ADMIN", "USER")
 //			.antMatchers(HttpMethod.POST, "/api/clientes").hasRole("ADMIN")
 //			.antMatchers("/api/clientes/**").hasRole("ADMIN")
 			.antMatchers("/h2-console/**").permitAll()
 			.anyRequest().authenticated() // all other urls can be access by any authenticated role
-			.and().headers().frameOptions().sameOrigin(); //allow use of frame to same origin urls. routes need authentication
+			.and().headers().frameOptions().sameOrigin()
+			.and().cors().configurationSource(corsConfigurationSource()); //allow use of frame to same origin urls. routes need authentication
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> filterRegistrationBean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+		filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return filterRegistrationBean;
 	}
 	
 }
