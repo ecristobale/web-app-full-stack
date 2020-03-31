382fc970bea5017faa6fd6f6150a81d1adfaf9fa
ecristobale <edu_ce_1988@hotmail.com>
2020-03-31 19:50:47 +0200

    Added security based on roles to the backend endpoints.

--- spring-boot-apirest/src/main/java/com/ecristobale/spring/boot/apirest/auth/ResourceServerConfig.java
+++ spring-boot-apirest/src/main/java/com/ecristobale/spring/boot/apirest/auth/ResourceServerConfig.java
@@ -14,7 +14,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	// Security rules of our resources endpoints
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/clientes").permitAll() // public route
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/clientes", "/api/clientes/page/**", "/api/uploads/img/**").permitAll() // public route
+			.antMatchers(HttpMethod.GET, "/api/clientes/{id}").hasAnyRole("ADMIN", "USER")
+			.antMatchers(HttpMethod.POST, "/api/clientes/upload").hasAnyRole("ADMIN", "USER")
+			.antMatchers(HttpMethod.POST, "/api/clientes").hasRole("ADMIN")
+			.antMatchers("/api/clientes/**").hasRole("ADMIN")
 			.antMatchers("/h2-console/**").permitAll()
 			.anyRequest().authenticated() // all other urls can be access by any authenticated role
 			.and().headers().frameOptions().sameOrigin(); //allow use of frame to same origin urls. routes need authentication
--- spring-boot-apirest/src/main/java/com/ecristobale/spring/boot/apirest/models/services/UsuarioService.java
+++ spring-boot-apirest/src/main/java/com/ecristobale/spring/boot/apirest/models/services/UsuarioService.java
@@ -45,6 +45,7 @@ public class UsuarioService implements UserDetailsService, IUsuarioService {
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Usuario findByUsername(String username) {
 		return usuarioDao.findByUsername(username);
 	}
