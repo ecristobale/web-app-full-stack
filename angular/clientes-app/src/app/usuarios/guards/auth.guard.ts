7b585b8df3ed4a7393f297122798bea58b7d56f1
ecristobale <edu_ce_1988@hotmail.com>
2020-04-04 03:15:36 +0200

    Configured auth guard to check if a token is expired and, in that case, redirect the user to login page

--- angular/clientes-app/src/app/usuarios/guards/auth.guard.ts
+++ angular/clientes-app/src/app/usuarios/guards/auth.guard.ts
@@ -14,9 +14,21 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
       if (this.authService.isAuthenticated()) {
+        if (this.isTokenExpired()) {
+          this.authService.logout();
+          this.router.navigate(['login']);
+          return false;
+        }
         return true;
       }
       this.router.navigate(['/login']);
       return false;
     }
+
+  isTokenExpired(): boolean {
+    let token = this.authService.token;
+    let payload = this.authService.obtainPayload(token);
+    let nowInSeconds = new Date().getTime() / 1000;
+    return (payload.exp < nowInSeconds);
+  }
 }
