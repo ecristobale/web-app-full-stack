dd8bdd92aac428f26527456517c5e60cf0ce2919
ecristobale <edu_ce_1988@hotmail.com>
2020-04-04 02:44:07 +0200

    Use AuthGuard to manage if the user can access to endpoints based on its authentication

--- angular/clientes-app/src/app/app-routing.module.ts
+++ angular/clientes-app/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { ClientesComponent } from './clientes/clientes.component';
 import { FormComponent } from './clientes/form.component';
 import { DetalleComponent } from './clientes/detalle/detalle.component';
 import { LoginComponent } from './usuarios/login.component';
+import { AuthGuard } from './usuarios/guards/auth.guard';
 
 const routes: Routes = [
     { path: '', redirectTo: '/clientes', pathMatch: 'full' },
     { path: 'clientes', component: ClientesComponent },
     { path: 'clientes/page/:page', component: ClientesComponent },
-    { path: 'clientes/form', component: FormComponent },
-    { path: 'clientes/form/:id', component: FormComponent },
+    { path: 'clientes/form', component: FormComponent, canActivate: [AuthGuard] },
+    { path: 'clientes/form/:id', component: FormComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent }
   ];
 
