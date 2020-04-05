9deee0afa842e519062299e4afffa7ca727ab495
ecristobale <edu_ce_1988@hotmail.com>
2020-04-06 01:08:11 +0200

    Autocomplete product name with names retrieved from backend.

--- angular/clientes-app/src/app/facturas/facturas.component.ts
+++ angular/clientes-app/src/app/facturas/facturas.component.ts
@@ -4,7 +4,9 @@ import { ClienteService } from '../clientes/cliente.service';
 import { ActivatedRoute } from '@angular/router';
 import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
+import {map, flatMap} from 'rxjs/operators';
+import { FacturaService } from './services/factura.service';
+import { Producto } from './models/producto';
 
 @Component({
   selector: 'app-facturas',
@@ -14,11 +16,11 @@ export class FacturasComponent implements OnInit {
   titulo = 'Nueva Factura';
   factura: Factura = new Factura();
   autocompleteControl = new FormControl();
-  productos: string[] = ['Mesa', 'Tablet', 'Sony', 'Samsung', 'Tv LG', 'Bicicleta' ];
-  productosFiltrados: Observable<string[]>;
+  productosFiltrados: Observable<Producto[]>;
 
   constructor(private clienteService: ClienteService,
-              private activatedRoute: ActivatedRoute) { }
+              private activatedRoute: ActivatedRoute,
+              private facturaService: FacturaService) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(params => {
@@ -27,15 +29,19 @@ export class FacturasComponent implements OnInit {
     });
     this.productosFiltrados = this.autocompleteControl.valueChanges
     .pipe(
-      startWith(''),
-      map(value => this._filter(value))
+      map(value => typeof value === 'string' ? value : value.nombre),
+      flatMap(value => value ? this._filter(value) : [])
     );
   }
 
-  private _filter(value: string): string[] {
+  private _filter(value: string): Observable<Producto[]> {
     const filterValue = value.toLowerCase();
 
-    return this.productos.filter(option => option.toLowerCase().includes(filterValue));
+    return this.facturaService.productFilter(filterValue);
+  }
+
+  showProductName(producto?: Producto): string | undefined {
+    return producto ? producto.nombre : undefined;
   }
 
 }
--- angular/clientes-app/src/app/facturas/services/factura.service.ts
+++ angular/clientes-app/src/app/facturas/services/factura.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Factura } from '../models/factura';
+import { Producto } from '../models/producto';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,9 @@ export class FacturaService {
 
   deleteFactura(id: number): Observable<void> {
     return this.http.delete<void>(`${this.urlEndpoint}/${id}`);
+  }
+
+  productFilter(term: string): Observable<Producto[]> {
+    return this.http.get<Producto[]>(`${this.urlEndpoint}/product-filter/${term}`);
   }
 }
