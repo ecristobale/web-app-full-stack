470c88d203fb3fe35002395319a35dd61163c9e3
ecristobale <edu_ce_1988@hotmail.com>
2020-04-10 16:44:29 +0200

    Changed endpoint

--- angular/clientes-app/src/app/config/config.ts
+++ angular/clientes-app/src/app/config/config.ts
@@ -1,2 +1,2 @@
-//export const URL_BACKEND = 'https://spring-boot-2clientes-app.herokuapp.com';
-export const URL_BACKEND = 'http://localhost:8081';
+export const URL_BACKEND = 'https://spring-boot-2clientes-app.herokuapp.com';
+//export const URL_BACKEND = 'http://localhost:8081';
